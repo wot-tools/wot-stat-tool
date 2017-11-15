d41228ef21153f1c249078a4046fe6b35e35ce19
sKanoodle <sKanoodle@users.noreply.github.com>
2017-11-15 15:09:43 +0100

    hiding filtered rows instead of rebuilding the grid

--- WoTStatTool/ViewModel/TankStatisticsViewModel.cs
+++ WoTStatTool/ViewModel/TankStatisticsViewModel.cs
@@ -83,7 +83,24 @@ namespace WotStatsTool.ViewModel
         public string Type => Tank?.VehicleType.ToString();
         public int Tier => Tank?.Tier ?? -1;
 
-        public bool MeetsFilterCriteria(TankFilter filter)
+        private bool _IsHidden;
+        public bool IsHidden
+        {
+            get => _IsHidden;
+            set
+            {
+                if (_IsHidden == value) return;
+                _IsHidden = value;
+                OnPropertyChanged(nameof(IsHidden));
+            }
+        }
+
+        public void ApplyFilter(TankFilter filter)
+        {
+            IsHidden = !MeetsFilterCriteria(filter);
+        }
+
+        private bool MeetsFilterCriteria(TankFilter filter)
         {
             if (Tank == null)
                 return false;
