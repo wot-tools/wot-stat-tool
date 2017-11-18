a1b5fbdb813a752fbc4e2f8495f4414ea9ee0503
Unknown <accounts_aimdrol@hotmail.com>
2017-11-19 00:07:12 +0100

    Added WinrateColor and WinrateForegroundColor converters

--- WoTStatTool/MainWindow.xaml.cs
+++ WoTStatTool/MainWindow.xaml.cs
@@ -265,6 +265,91 @@ namespace WotStatsTool
         }
     }
 
+
+    public class WinrateColorConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instance = new WinrateColorConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double val;
+            if (String.IsNullOrWhiteSpace(value as string))
+                val = 0;
+            else if (value as string == "NaN")
+                val = double.NaN;
+            else
+            {
+                var winrateString = value as string;
+                val = double.Parse(winrateString.Substring(0, winrateString.Length - 2), culture.NumberFormat) / 100;
+            }
+
+            Func<int, SolidColorBrush> fromHex = hex =>
+            {
+                return new SolidColorBrush(Color.FromRgb((byte)(hex >> 16), (byte)(hex >> 8), (byte)hex));
+            };
+
+            switch (val)
+            {
+                case double testVal when double.IsNaN(testVal): return null;
+                case double testVal when testVal < 0.46: return fromHex((int)WinrateColors.VeryBad);
+                case double testVal when testVal < 0.47: return fromHex((int)WinrateColors.Bad);
+                case double testVal when testVal < 0.48: return fromHex((int)WinrateColors.BelowAverage);
+                case double testVal when testVal < 0.50: return fromHex((int)WinrateColors.Average);
+                case double testVal when testVal < 0.52: return fromHex((int)WinrateColors.AboveAverage);
+                case double testVal when testVal < 0.54: return fromHex((int)WinrateColors.Good);
+                case double testVal when testVal < 0.56: return fromHex((int)WinrateColors.VeryGood);
+                case double testVal when testVal < 0.60: return fromHex((int)WinrateColors.Great);
+                case double testVal when testVal < 0.65: return fromHex((int)WinrateColors.Unicum);
+                case double testVal: return fromHex((int)WinrateColors.SuperUnicum);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WinrateForegroundColorConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instance = new WinrateForegroundColorConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double val;
+            if (String.IsNullOrWhiteSpace(value as string))
+                val = 0;
+            else if (value as string == "NaN")
+                val = double.NaN;
+            else
+            {
+                var winrateString = value as string;
+                val = double.Parse(winrateString.Substring(0, winrateString.Length - 2), culture.NumberFormat) / 100;
+            }
+
+
+            switch (val)
+            {
+                case double testVal when double.IsNaN(testVal): return Brushes.Black;
+                case double testVal when testVal < 0.46: return Brushes.White;
+                case double testVal when testVal < 0.47: return Brushes.White;
+                case double testVal when testVal < 0.48: return Brushes.White;
+                case double testVal when testVal < 0.50: return Brushes.Black;
+                case double testVal when testVal < 0.52: return Brushes.Black;
+                case double testVal when testVal < 0.54: return Brushes.White;
+                case double testVal when testVal < 0.56: return Brushes.Black;
+                case double testVal when testVal < 0.60: return Brushes.White;
+                case double testVal when testVal < 0.65: return Brushes.White;
+                case double testVal: return Brushes.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class IsLessThanConverter : IValueConverter
     {
         public static readonly IValueConverter Instance = new IsLessThanConverter();
