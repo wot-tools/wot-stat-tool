b9d3001d8d9c2b7762aab6f40dea4c4b31662bf2
Aimdrol <accounts_aimdrol@hotmail.com>
2017-11-22 01:11:52 +0100

    Added DeltaForegroundConverter

---
+++ WoTStatTool/Converters/DeltaForegroundConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WotStatsTool.Converters
+{
+    public class DeltaForegroundConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instance = new DeltaForegroundConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double val = (double)value;
+
+            switch (val)
+            {
+                case double testVal when testVal > 0: return Brushes.MediumSeaGreen;
+                case double testVal when testVal < 0: return Brushes.Tomato;
+                case double testVal: return Brushes.Gray;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
